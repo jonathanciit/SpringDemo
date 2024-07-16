823b5294b2ff5b1d14813b355dee1c8b78377089
JunWick <jguller20223@gmail.com>
2024-07-16 16:52:58 +0800

    test

--- check_if_up.ps1
+++ check_if_up.ps1
@@ -1,11 +1,16 @@
 # Define the URL of the application
-$AppUrl = "http://localhost:8888"
+$AppUrl = "http://localhost:8989"
 
 # Make an HTTP request using curl for debugging
 try {
+    Start-Sleep -Seconds 5
     $response = & curl -Uri $AppUrl
     Write-Host "Response: $response"
 } catch {
     Write-Host ("Error: " + $_.Exception.Message)
     exit 1
 }
+
+
+
+#powershell.exe -File "C:\Users\Admin\IdeaProjects\SpringDemo\check_if_up.ps1"
