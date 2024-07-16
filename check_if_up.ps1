55d1eaa7e611bbac8e162b7f0a6a481c63a9cb6a
JunWick <jguller20223@gmail.com>
2024-07-16 15:47:31 +0800

    test

--- check_if_up.ps1
+++ check_if_up.ps1
@@ -1,13 +1,18 @@
 # Define the URL of the application
 $AppUrl = "http://localhost:8888"
 
-# Create a WebClient object
-$webClient = New-Object System.Net.WebClient
+# Ignore SSL certificate errors (not recommended for production)
+[System.Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}
 
-# Make HTTP request
+# Make an HTTP request to the application's health endpoint or root URL
 try {
-    $response = $webClient.DownloadString($AppUrl)
-    Write-Host "Application is accessible."
+    $Response = Invoke-WebRequest -Uri $AppUrl -UseBasicParsing
+    if ($Response.StatusCode -eq 200) {
+        Write-Host "Application is accessible."
+    } else {
+        Write-Host ("Application is not accessible. HTTP response code: " + $Response.StatusCode)
+        exit 1
+    }
 } catch {
     Write-Host ("Application is not accessible. Error: " + $_.Exception.Message)
     exit 1
